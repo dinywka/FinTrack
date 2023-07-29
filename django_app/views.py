462a5a1c25112c4b7cbf5c451ee1ba3b2f090b43
Dina <73873857+dinywka@users.noreply.github.com>
2023-07-29 13:37:29 +0600

    updeate

--- django_app/views.py
+++ django_app/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse, HttpResponseNotAllowed
 import re
 from django.contrib.auth import authenticate, login, logout
@@ -7,6 +6,8 @@ from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from .models import ExpensesCategory, IncomesAccount, Expense, Income
+from itertools import chain
+
 
 def home(request):
     return render(request, 'django_app/home.html')
@@ -66,60 +67,137 @@ def logout_f(request: HttpRequest) -> HttpResponse:
     logout(request)
     return redirect(reverse('login'))
 
+#
+# def main(request):
+#     incomes_accounts = None
+#     expenses_categories = None
+#     incomes = None
+#     expenses = None
+#     if request.user.is_authenticated:
+#         incomes_accounts = IncomesAccount.objects.filter(user=request.user)
+#         expenses_categories = ExpensesCategory.objects.filter(user=request.user)
+#         incomes = Income.objects.filter(user=request.user)
+#         expenses = Expense.objects.filter(user=request.user)
+#
+#         if request.method == 'POST':
+#             # Check if it's an income or an expense
+#             if 'account' in request.POST:  # This is an income
+#                 # Get the submitted data
+#                 sum = request.POST.get('sum')
+#                 account_id = request.POST.get('account')
+#                 print(f"Sum: {request.POST.get('sum')}")
+#
+#                 # Fetch the account from the database
+#                 account = IncomesAccount.objects.get(id=account_id)
+#
+#                 # Create a new Income object and save it to the database
+#                 income = Income(user=request.user, sum=sum, account=account)
+#                 income.save()
+#
+#             elif 'category' in request.POST:  # This is an expense
+#                 # Get the submitted data
+#                 sum = request.POST.get('sum')
+#                 category_id = request.POST.get('category')
+#
+#                 # Fetch the category from the database
+#                 category = ExpensesCategory.objects.get(id=category_id)
+#
+#                 # Create a new Expense object and save it to the database
+#                 expense = Expense(user=request.user, sum=sum, category=category)
+#                 expense.save()
+#
+#             #todo
+#             elif 'new_account' in request.POST:
+#                 title = request.POST.get('title', None)
+#                 IncomesAccount.objects.create(user=request.user, title=title)
+#                 return redirect('main')
+#
+#             elif 'new_expense_category' in request.POST:
+#                 title = request.POST.get('title', None)
+#                 ExpensesCategory.objects.create(user=request.user, title=title)
+#                 return redirect('main')
+#
+#             # Redirect to the same page after form submission
+#             return render(request, 'django_app/main.html', {
+#                 'incomes_accounts': incomes_accounts,
+#                 'expenses_categories': expenses_categories,
+#                 'incomes': incomes,
+#                 'expenses': expenses,
+#             })
+#
+#     else:
+#         incomes_accounts = None
+#         expenses_categories = None
+#
+#     return render(request, 'django_app/main.html', {
+#         'incomes_accounts': incomes_accounts,
+#         'expenses_categories': expenses_categories,
+#         'incomes': incomes,
+#         'expenses': expenses,
+#     })
+
 
 def main(request):
+    if request.method == 'POST':
+        if 'account' in request.POST:  # This is an income
+                        # Get the submitted data
+            sum = request.POST.get('sum')
+            account_id = request.POST.get('account')
+            print(f"Sum: {request.POST.get('sum')}")
+
+                        # Fetch the account from the database
+            account = IncomesAccount.objects.get(id=account_id)
+
+                        # Create a new Income object and save it to the database
+            income = Income(user=request.user, sum=sum, account=account)
+            income.save()
+
+        elif 'category' in request.POST:  # This is an expense
+                        # Get the submitted data
+            sum = request.POST.get('sum')
+            category_id = request.POST.get('category')
+
+                        # Fetch the category from the database
+            category = ExpensesCategory.objects.get(id=category_id)
+
+                        # Create a new Expense object and save it to the database
+            expense = Expense(user=request.user, sum=sum, category=category)
+            expense.save()
+
+                    #todo
+        elif 'new_account' in request.POST:
+            title = request.POST.get('title', None)
+            IncomesAccount.objects.create(user=request.user, title=title)
+            return redirect('main')
+
+        elif 'new_expense_category' in request.POST:
+            title = request.POST.get('title', None)
+            ExpensesCategory.objects.create(user=request.user, title=title)
+            return redirect('main')
+        return redirect('main')
+
+    incomes_accounts = None
+    expenses_categories = None
+    incomes = None
+    expenses = None
     if request.user.is_authenticated:
         incomes_accounts = IncomesAccount.objects.filter(user=request.user)
         expenses_categories = ExpensesCategory.objects.filter(user=request.user)
         incomes = Income.objects.filter(user=request.user)
         expenses = Expense.objects.filter(user=request.user)
+        table_incomes = [(income, 'income') for income in Income.objects.filter(user=request.user)]
+        table_expenses = [(expense, 'expense') for expense in Expense.objects.filter(user=request.user)]
 
-        if request.method == 'POST':
-            # Check if it's an income or an expense
-            if 'account' in request.POST:  # This is an income
-                # Get the submitted data
-                sum = request.POST.get('sum')
-                account_id = request.POST.get('account')
-                print(f"Sum: {request.POST.get('sum')}")
-
-                # Fetch the account from the database
-                account = IncomesAccount.objects.get(id=account_id)
-
-                # Create a new Income object and save it to the database
-                income = Income(user=request.user, sum=sum, account=account)
-                income.save()
-
-            elif 'category' in request.POST:  # This is an expense
-                # Get the submitted data
-                sum = request.POST.get('sum')
-                category_id = request.POST.get('category')
-
-                # Fetch the category from the database
-                category = ExpensesCategory.objects.get(id=category_id)
-
-                # Create a new Expense object and save it to the database
-                expense = Expense(user=request.user, sum=sum, category=category)
-                expense.save()
-
-            # Redirect to the same page after form submission
-            return render(request, 'django_app/main.html', {
-                'incomes_accounts': incomes_accounts,
-                'expenses_categories': expenses_categories,
-                'incomes': incomes,
-                'expenses': expenses,
-            })
-
-    else:
-        incomes_accounts = None
-        expenses_categories = None
+        transactions = list(chain(table_expenses, table_incomes))
+        transactions.sort(key=lambda x: x[0].datetime, reverse=True)
 
     return render(request, 'django_app/main.html', {
+        'transactions': transactions,
         'incomes_accounts': incomes_accounts,
         'expenses_categories': expenses_categories,
         'incomes': incomes,
         'expenses': expenses,
     })
-
 
 
 
--- static/js/my.js
+++ static/js/my.js
@@ -31,3 +31,74 @@ popup.addEventListener('click', (e) => {
     }
 
 });
+
+
+// Get the button that opens the modal
+var btn = document.querySelector('.popupbtn-acc');
+
+// Get the modal
+var modal = document.querySelector('#add-account-popup');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("popup-close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+
+const expensePopupBtn = document.querySelector('.add-expense-acc');
+const expensePopup = document.querySelector('#add-expense-popup');
+const expensePopupClose = expensePopup.querySelector('.popup-close');
+
+expensePopupBtn.addEventListener('click',() => {
+    expensePopup.style.display = "block";
+    console.log("opening expense modal");
+});
+
+expensePopupClose.addEventListener('click', () => {
+    expensePopup.style.display = 'none';
+});
+
+expensePopup.addEventListener('click', (e) => {
+    if(e.target.className === 'popup-wrapper'){
+      expensePopup.style.display = 'none';
+    }
+});
+
+
+const expenseCategoryPopupBtn = document.querySelector('.add-expense-category-button');
+const expenseCategoryPopup = document.querySelector('#add-expense-category-popup');
+const expenseCategoryPopupClose = expenseCategoryPopup.querySelector('.popup-close');
+
+expenseCategoryPopupBtn.addEventListener('click',() => {
+    expenseCategoryPopup.style.display = "block";
+    console.log("opening expense category modal");
+});
+
+expenseCategoryPopupClose.addEventListener('click', () => {
+    expenseCategoryPopup.style.display = 'none';
+});
+
+expenseCategoryPopup.addEventListener('click', (e) => {
+    if(e.target.className === 'popup-wrapper'){
+      expenseCategoryPopup.style.display = 'none';
+    }
+});
+
+
